ed34d73a13e17e64a383d9834b130556d6b8f888
Livmiller022 <99272520+Livmiller022@users.noreply.github.com>
2022-09-22 12:53:29 -0400

    hw 1 correct submit

---
+++ Module 01/homework/problem3.java
@@ -0,0 +1,13 @@
+package Module 01.homework;
+
+public class problem3 {
+  
+    public static void main(String[] args) { 
+ 
+        System.out.println("  O    LL  ii v     v  ii     a "); 
+        System.out.println("O   O  LL  II  v   v   II    a a"); 
+        System.out.println("O   O  LL  II   V V    II   aaaaa"); 
+        System.out.println("  0    LL  II    V     II  a     a"); 
+    } 
+} 
+ 
