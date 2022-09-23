5bba12fc9bbacd7e1ae82291de5741922e9e4a52
Livmiller022 <99272520+Livmiller022@users.noreply.github.com>
2022-09-23 13:40:44 -0400

    problem2 homework2

---
+++ Module 02/homework/problem2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Test{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the friction force in Newtons: ");
+		double F=sc.nextDouble();
+		System.out.print("Enter the object's mass in kg: ");
+		double m=sc.nextDouble();
+		System.out.print("Enter the object's acceleration in m/s^2: ");
+		double a=sc.nextDouble();
+		double g=9.8;
+		//compute the coefficient of friction
+		double u =(F-m*a)/(m*g);
+		System.out.printf("The coefficient of friction is %.6f",u);
