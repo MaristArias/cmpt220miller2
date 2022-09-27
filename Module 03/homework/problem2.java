05f5f2b7f139ef2952677d51ff5bdbf5909fd3d6
Livmiller022 <99272520+Livmiller022@users.noreply.github.com>
2022-09-27 14:45:40 -0400

    problem 2 homework 3

---
+++ Module 03/homework/problem2.java
@@ -0,0 +1,26 @@
+package Module 03.homework;
+
+public class problem2 {
+    import java.util.Scanner;
+public class module03_02Extra {
+  public static void main(String args[]) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a numerator: ");
+    int numerator = input.nextInt();
+    System.out.print("Enter a denominator: ");
+    int denominator = input.nextInt();
+    if (numerator < denominator) {
+      System.out.println(numerator + " / " + denominator + " is a proper fraction"); 
+    }
+    else if (numerator % denominator == 0) {
+      System.out.print(numerator + " / " + denominator + " is an improper fraction "); 
+      System.out.println("and it can be reduced to " + numerator / denominator); 
+    }
+    else {
+      System.out.print(numerator + " / " + denominator + " is an improper fraction "); 
+      System.out.println("and its mixed fraction is " + numerator / denominator + " + " +  
+          numerator % denominator + " / " + denominator); 
+    }
+  }
+}
+}
