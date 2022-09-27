7c6ab088765c2c293e00279702daae0baba11366
Livmiller022 <99272520+Livmiller022@users.noreply.github.com>
2022-09-27 14:40:56 -0400

    problem 1 homework 3

---
+++ Module 01/homework/problem1.java
@@ -0,0 +1,9 @@
+package Module 01.homework;
+
+public class problem1 {
+    public static void main(String[] args) {   
+
+        String str = "(3*x^2) + (2*x)+ 4 = "; 
+        int x = Integer.parseInt( str.solve("x") ) //==> 7;  
+}
+}
