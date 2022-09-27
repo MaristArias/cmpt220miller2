cc020f8cb0ddfb92b497e4a6504e7c4a72490e58
Juan Arias <juan.arias@marist.edu>
2022-09-27 13:54:57 -0400

    Feedback

--- Module 02/homework/problem1.java
+++ Module 02/homework/problem1.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 
-class Test
-{
-public static void main(String args[])
-{
-    double radius, diameter, circumference, area;
-    Scanner op=new Scanner(System.in);
-    /*
-     * Input radius of circle from user
-     */
-    System.out.print("Enter radius of circle: ");
-    radius=op.nextDouble();
+// Be careful with the class names. They should match the 
+// filenames
+public class Problem1 {
+    public static void main(String args[]) {
+        double radius, diameter, circumference, area;
+        Scanner op = new Scanner(System.in);
+        /*
+         * Input radius of circle from user
+         */
+        System.out.print("Enter radius of circle: ");
+        radius = op.nextDouble();
 
-    /*
-     * Calculate diameter, circumference and area
-     */
-    diameter = 2 * radius;
-    circumference = 2 * 3.14 * radius;
-    area = 3.14 * (radius * radius);
+        /*
+         * Calculate diameter, circumference and area
+         */
+        diameter = 2 * radius;
+        circumference = 2 * 3.14 * radius;
+        area = 3.14 * (radius * radius);
 
-    /*
-     * Print all results
-     */
-    System.out.println("Diameter of circle = "+diameter+" units ");
-    System.out.println("Circumference of circle = "+circumference+" units ");
-    System.out.println("Area of circle = "+area+" sq. units ");
+        /*
+         * Print all results
+         */
+        System.out.println("Diameter of circle = " + diameter + " units ");
+        System.out.println("Circumference of circle = " + circumference + " units ");
+        System.out.println("Area of circle = " + area + " sq. units ");
     }
 }
--- Module 02/homework/problem2.java
+++ Module 02/homework/problem2.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
-class Test{
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
+
+public class Problem2 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the friction force in Newtons: ");
-		double F=sc.nextDouble();
+		double F = sc.nextDouble(); /// Always use lowercase for variables unless they are constants
 		System.out.print("Enter the object's mass in kg: ");
-		double m=sc.nextDouble();
+		double m = sc.nextDouble();
 		System.out.print("Enter the object's acceleration in m/s^2: ");
-		double a=sc.nextDouble();
-		double g=9.8;
-		//compute the coefficient of friction
-		double u =(F-m*a)/(m*g);
-		System.out.printf("The coefficient of friction is %.6f",u);
-    }
+		double a = sc.nextDouble();
+		double g = 9.8;
+		// compute the coefficient of friction
+		double u = (F - m * a) / (m * g);
+		System.out.printf("The coefficient of friction is %.6f", u);
+	}
 }
