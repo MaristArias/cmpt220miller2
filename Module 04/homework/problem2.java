1750b45f0de9e47ec4817eb99732d76478b0484f
Livmiller022 <99272520+Livmiller022@users.noreply.github.com>
2022-09-29 18:14:41 -0400

    problem2 homework 4

---
+++ Module 04/homework/problem2.java
@@ -0,0 +1,27 @@
+package Module 04.homework;
+
+    import java.util.Scanner;
+
+public class problem2{
+
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of terms to calculate the value of π");
+        int n=sc.nextInt();
+
+        double pi=0;
+        double sign=1.0;
+
+        for(int i =0;i<n;i++)
+        {
+            pi+=sign/((2*i)+1);
+        //At every next element,sign will change
+            sign*=-1;    //sign=sign*(-1);
+        }
+        System.out.println(pi*4);
+    }
+
+}
+
