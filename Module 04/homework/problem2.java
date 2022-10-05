1c8094a63ed7a33fd4540e75e8865a9f446008f9
Livmiller022 <99272520+Livmiller022@users.noreply.github.com>
2022-10-05 11:34:54 -0400

    update problem 2

--- Module 04/homework/problem2.java
+++ Module 04/homework/problem2.java
@@ -3,8 +3,6 @@ package Module 04.homework;
     import java.util.Scanner;
 
 public class problem2{
-
-
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
