fcaededdb09bae675e491496dd52e0280675b425
Juan Arias <juan.arias@marist.edu>
2022-10-15 23:34:31 -0400

    Feedback

--- Module 05/homework/problem2.java
+++ Module 05/homework/problem2.java
@@ -1,29 +1,24 @@
-package Module 05.homework;
-public static String reverseCase(String s)
-{
-    //copy into the character array
-    char str[] = s.toCharArray();
-    
-    //change the case
-    for(int i=0; i<str.length; i++)
-    {
-        if(Character.isUpperCase(str[i]))
-        {
-            str[i] = Character.toLowerCase(str[i]);
-        }
-        else if(Character.isLowerCase(str[i]))
-        {
-            str[i] = Character.toUpperCase(str[i]);
-        }
-        else
-            {
+//package Module 05.homework;
+public class problem2 {
+    public static String reverseCase(String s) {
+        // copy into the character array
+        char str[] = s.toCharArray();
+
+        // change the case
+        for (int i = 0; i < str.length; i++) {
+            if (Character.isUpperCase(str[i])) {
+                str[i] = Character.toLowerCase(str[i]);
+            } else if (Character.isLowerCase(str[i])) {
+                str[i] = Character.toUpperCase(str[i]);
+            } else {
                 str[i] = str[i];
             }
+        }
+
+        // copy str to s
+        s = new String(str);
+
+        // return statement
+        return s;
     }
-    
-    //copy str to s
-    s = new String(str);
-    
-    //return statement
-    return s;
 }
